282696d60537e9fa80480a553b4d0c808774be2e
Gustavo Borges <gustavoborges@sisconsultoria.com.br>
2022-06-30 20:41:30 -0300

    Exercício 15 e 16 - aula 15

---
+++ src/com/loiane/cursojava/aula15/labs/Exer15.java
@@ -0,0 +1,40 @@
+package com.loiane.cursojava.aula15.labs;
+
+import java.util.Scanner;
+
+public class Exer15 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite o valor do lado A do triângulo: ");
+		double A = scan.nextDouble();
+
+		System.out.println("Digite o valor do lado B do triângulo: ");
+		double B = scan.nextDouble();
+
+		System.out.println("Digite o valor do lado C do triângulo: ");
+		double C = scan.nextDouble();
+
+		if ( A + B < C) {
+			System.out.println("É um triângulo");
+		} else if(A + C < B) {
+			System.out.println("É um triângulo");
+		} else if( B + C < C){
+			System.out.println("É um triângulo");
+		} else {
+			System.out.println("Não é um triângulo");
+		}
+
+		if (A == B && A == C && B == C) {
+			System.out.println("Triângulo Equilátero: três lados iguais");
+		} else if ( (A == B) || (A == C) || (B == C) ) {
+			System.out.println("Triângulo Isósceles: quaisquer dois lados iguais");
+		} else if ( (A != B) && (A != C) && (B != C) ) {
+			System.out.println("Triângulo Escaleno: três lados diferentes");
+		}
+		scan.close();
+	}
+
+}
---
+++ src/com/loiane/cursojava/aula15/labs/Exer16.java
@@ -0,0 +1,47 @@
+package com.loiane.cursojava.aula15.labs;
+
+import java.util.Scanner;
+
+public class Exer16 {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o valor de A: ");
+		double A = scan.nextDouble();
+		
+		if(A == 0) {
+			System.out.println("A equação não é do segundo grau");
+			System.out.println("Fim do programa.");
+			System.exit(0);
+		}
+
+		System.out.println("Digite o valor de B: ");
+		double B = scan.nextDouble();
+
+		System.out.println("Digite o valor de C: ");
+		double C = scan.nextDouble();
+		
+		double delta = Math.pow(B, 2) - 4 * A * C;
+		
+		if (delta < 0) {
+			System.out.println("Se o delta calculado for negativo, a equação não possui raizes reais.");
+			System.out.println("Valor de delta: " + delta);
+			System.out.println("Fim do programa.");
+			System.exit(0);
+		} else if(delta == 0) {
+			System.out.println("A equação possui apenas uma raiz real");
+			System.out.println("Valor de delta: " + delta);
+			System.out.println("Fim do programa.");
+			System.exit(0);
+		} else {
+			System.out.println("A equação possui duas raiz reais");
+			System.out.println("Valor de delta: " + delta);
+			System.out.println("Fim do programa.");
+			System.exit(0);
+		}
+		
+		scan.close();
+		
+	}
+}
